d095306db893e8f708922db4fa610ce3e91b0def
tux78 <40579446+tux78@users.noreply.github.com>
2021-01-31 16:28:49 +0100

    renamed/updated
    Added option to roll-out policy for dedicated data source only (based on data source ID)

--- esm_PolicyRollout.ps1
+++ esm_PolicyRollout.ps1
@@ -1,41 +1,51 @@
 
 cls
 add-type @"
-    using System.Net;
-    using System.Security.Cryptography.X509Certificates;
-    public class TrustAllCertsPolicy : ICertificatePolicy {
-        public bool CheckValidationResult(
-            ServicePoint srvPoint, X509Certificate certificate,
-            WebRequest request, int certificateProblem) {
-            return true;
-        }
-    }
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-############################## 
+############################## 
 # Change variables accordingly
 ##############################
 
+# IP or FQDN of local ESM
 $global:esmIP = '10.10.10.10'
 
-########################### 
+# DS ID of any ERC/ACE to have policy rolled out
+# if empty or comment (#), ALL data sources will have the policy rolled out
+# $global:ercID = '144116287587483648'
+
+########################### 
 # Some Inits
 ###########################
 $global:esmAPI = 'https://' + $global:esmIP + '/rs/esm/v2/'
 $global:headers = $null
 $global:cookie = $null
 
-########################### 
+########################### 
 # Main Function
 ###########################
 Function Main
 {
   Login;
-  $response = devGetDeviceList @('RECEIVER')
-  $response = $response | ConvertFrom-Json
-  write-host $response
+  $response = ''
+  if ($null -eq $global:ercID -or $global:ercID -eq '')
+  {
+    $response = devGetDeviceList @('RECEIVER')
+    $response = $response | ConvertFrom-Json
+  } else {
+    $response = '{"id" : "' + $ercID + '"}' | convertfrom-json
+  }
   Foreach ($erc in $response)
   {
     $datasources = dsGetDataSourceList $erc.id
@@ -44,6 +54,7 @@ Function Main
     $dslist = @()
     Foreach ($ds in $datasources)
     {
+      Write-Host 'Policy rollOut for data source: ' $ds.name ' (' $ds.id ')'
       $dslist += $ds.id
     }
     $rollout = plcyRollPolicy $dslist
@@ -54,7 +65,7 @@ Function Main
 # ESM API IMPLEMENTATION
 #################################################################################################
 
-########################### 
+########################### 
 # Get ESM Devices
 ###########################
 Function devGetDeviceList ($deviceType)
@@ -68,7 +79,7 @@ Function devGetDeviceList ($deviceType)
   return $response.Content 
 }
 
-########################### 
+########################### 
 # Get Data Sources per Device
 ###########################
 Function dsGetDataSourceList ($receiverID)
@@ -80,13 +91,13 @@ Function dsGetDataSourceList ($receiverID)
   return $response.Content 
 }
 
-########################### 
+########################### 
 # Roll Out Policy
 ###########################
 Function plcyRollPolicy ($dsID)
 {
   $params = @{
-    ids = $dsID
+    ids = $dsID
   };
   $response = CallEsmApi 'plcyRollPolicy' $params
   return $response.Content 
@@ -96,7 +107,7 @@ Function plcyRollPolicy ($dsID)
 # HELPER FUNCTIONS
 #################################################################################################
 
-########################### 
+########################### 
 # Login
 ###########################
 Function Login
@@ -106,31 +117,31 @@ Function Login
   $username = $cred.GetNetworkCredential().Username
   $passwd = $cred.GetNetworkCredential().Password
   $login_url = $esmhost + "login"
- 
+ 
   $b64_user = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($username))
   $b64_passwd = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($passwd))
 
 
   $params = @{
-        username = $b64_user
-        password = $b64_passwd
-        locale = 'en_US'
-        os = 'Win32'};        
+        username = $b64_user
+        password = $b64_passwd
+        locale = 'en_US'
+        os = 'Win32'};        
   $body = $params | ConvertTo-Json
- 
+ 
   $global:headers = @{
-    'Content-Type' = 'application/json'
-  };
- 
+    'Content-Type' = 'application/json'
+  };
+ 
   $login_headers = $global:headers
   $login_headers.Add("Authorization", "Basic "+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($username+":"+$passwd )))
   $response = Invoke-WebRequest $login_url -Method Post -Headers $login_headers -Body $body -SessionVariable global:cookie
- 
+ 
   $global:headers.Add('X-Xsrf-Token', $response.headers.Get_Item('Xsrf-Token'))
 
 }
 
-########################### 
+########################### 
 # Call ESM API
 ###########################
 Function CallEsmApi ([String]$method, $params)
